06c2cacc8e5a7b35b9a8282b1cd4df153114c93c
Ilia Kondrashov <i.kondrashov@corp.badoo.com>
2013-01-26 01:06:02 +0400

    remove namespace, move global functions to Utils class

--- src/SphinxSearchApi/Client.php
+++ src/SphinxSearchApi/Client.php
@@ -1,5 +1,4 @@
 <?php
-namespace SphinxApi;
 //
 // $Id: sphinxapi.php 3439 2012-10-10 05:47:48Z kevg $
 //
@@ -45,268 +44,8 @@ namespace SphinxApi;
 //  - return ints if we can
 //  - otherwise format number into a string
 
-/// pack 64-bit signed
-function sphPackI64 ( $v )
-{
-	assert ( is_numeric($v) );
-
-	// x64
-	if ( PHP_INT_SIZE>=8 )
-	{
-		$v = (int)$v;
-		return pack ( "NN", $v>>32, $v&0xFFFFFFFF );
-	}
-
-	// x32, int
-	if ( is_int($v) )
-		return pack ( "NN", $v < 0 ? -1 : 0, $v );
-
-	// x32, bcmath
-	if ( function_exists("bcmul") )
-	{
-		if ( bccomp ( $v, 0 ) == -1 )
-			$v = bcadd ( "18446744073709551616", $v );
-		$h = bcdiv ( $v, "4294967296", 0 );
-		$l = bcmod ( $v, "4294967296" );
-		return pack ( "NN", (float)$h, (float)$l ); // conversion to float is intentional; int would lose 31st bit
-	}
-
-	// x32, no-bcmath
-	$p = max(0, strlen($v) - 13);
-	$lo = abs((float)substr($v, $p));
-	$hi = abs((float)substr($v, 0, $p));
-
-	$m = $lo + $hi*1316134912.0; // (10 ^ 13) % (1 << 32) = 1316134912
-	$q = floor($m/4294967296.0);
-	$l = $m - ($q*4294967296.0);
-	$h = $hi*2328.0 + $q; // (10 ^ 13) / (1 << 32) = 2328
-
-	if ( $v<0 )
-	{
-		if ( $l==0 )
-			$h = 4294967296.0 - $h;
-		else
-		{
-			$h = 4294967295.0 - $h;
-			$l = 4294967296.0 - $l;
-		}
-	}
-	return pack ( "NN", $h, $l );
-}
-
-/// pack 64-bit unsigned
-function sphPackU64 ( $v )
-{
-	assert ( is_numeric($v) );
-
-	// x64
-	if ( PHP_INT_SIZE>=8 )
-	{
-		assert ( $v>=0 );
-
-		// x64, int
-		if ( is_int($v) )
-			return pack ( "NN", $v>>32, $v&0xFFFFFFFF );
-
-		// x64, bcmath
-		if ( function_exists("bcmul") )
-		{
-			$h = bcdiv ( $v, 4294967296, 0 );
-			$l = bcmod ( $v, 4294967296 );
-			return pack ( "NN", $h, $l );
-		}
-
-		// x64, no-bcmath
-		$p = max ( 0, strlen($v) - 13 );
-		$lo = (int)substr ( $v, $p );
-		$hi = (int)substr ( $v, 0, $p );
-
-		$m = $lo + $hi*1316134912;
-		$l = $m % 4294967296;
-		$h = $hi*2328 + (int)($m/4294967296);
-
-		return pack ( "NN", $h, $l );
-	}
-
-	// x32, int
-	if ( is_int($v) )
-		return pack ( "NN", 0, $v );
-
-	// x32, bcmath
-	if ( function_exists("bcmul") )
-	{
-		$h = bcdiv ( $v, "4294967296", 0 );
-		$l = bcmod ( $v, "4294967296" );
-		return pack ( "NN", (float)$h, (float)$l ); // conversion to float is intentional; int would lose 31st bit
-	}
-
-	// x32, no-bcmath
-	$p = max(0, strlen($v) - 13);
-	$lo = (float)substr($v, $p);
-	$hi = (float)substr($v, 0, $p);
-
-	$m = $lo + $hi*1316134912.0;
-	$q = floor($m / 4294967296.0);
-	$l = $m - ($q * 4294967296.0);
-	$h = $hi*2328.0 + $q;
-
-	return pack ( "NN", $h, $l );
-}
-
-// unpack 64-bit unsigned
-function sphUnpackU64 ( $v )
-{
-	list ( $hi, $lo ) = array_values ( unpack ( "N*N*", $v ) );
-
-	if ( PHP_INT_SIZE>=8 )
-	{
-		if ( $hi<0 ) $hi += (1<<32); // because php 5.2.2 to 5.2.5 is totally fucked up again
-		if ( $lo<0 ) $lo += (1<<32);
-
-		// x64, int
-		if ( $hi<=2147483647 )
-			return ($hi<<32) + $lo;
-
-		// x64, bcmath
-		if ( function_exists("bcmul") )
-			return bcadd ( $lo, bcmul ( $hi, "4294967296" ) );
-
-		// x64, no-bcmath
-		$C = 100000;
-		$h = ((int)($hi / $C) << 32) + (int)($lo / $C);
-		$l = (($hi % $C) << 32) + ($lo % $C);
-		if ( $l>$C )
-		{
-			$h += (int)($l / $C);
-			$l  = $l % $C;
-		}
-
-		if ( $h==0 )
-			return $l;
-		return sprintf ( "%d%05d", $h, $l );
-	}
-
-	// x32, int
-	if ( $hi==0 )
-	{
-		if ( $lo>0 )
-			return $lo;
-		return sprintf ( "%u", $lo );
-	}
-
-	$hi = sprintf ( "%u", $hi );
-	$lo = sprintf ( "%u", $lo );
-
-	// x32, bcmath
-	if ( function_exists("bcmul") )
-		return bcadd ( $lo, bcmul ( $hi, "4294967296" ) );
-
-	// x32, no-bcmath
-	$hi = (float)$hi;
-	$lo = (float)$lo;
-
-	$q = floor($hi/10000000.0);
-	$r = $hi - $q*10000000.0;
-	$m = $lo + $r*4967296.0;
-	$mq = floor($m/10000000.0);
-	$l = $m - $mq*10000000.0;
-	$h = $q*4294967296.0 + $r*429.0 + $mq;
-
-	$h = sprintf ( "%.0f", $h );
-	$l = sprintf ( "%07.0f", $l );
-	if ( $h=="0" )
-		return sprintf( "%.0f", (float)$l );
-	return $h . $l;
-}
-
-// unpack 64-bit signed
-function sphUnpackI64 ( $v )
-{
-	list ( $hi, $lo ) = array_values ( unpack ( "N*N*", $v ) );
-
-	// x64
-	if ( PHP_INT_SIZE>=8 )
-	{
-		if ( $hi<0 ) $hi += (1<<32); // because php 5.2.2 to 5.2.5 is totally fucked up again
-		if ( $lo<0 ) $lo += (1<<32);
-
-		return ($hi<<32) + $lo;
-	}
-
-	// x32, int
-	if ( $hi==0 )
-	{
-		if ( $lo>0 )
-			return $lo;
-		return sprintf ( "%u", $lo );
-	}
-	// x32, int
-	elseif ( $hi==-1 )
-	{
-		if ( $lo<0 )
-			return $lo;
-		return sprintf ( "%.0f", $lo - 4294967296.0 );
-	}
-
-	$neg = "";
-	$c = 0;
-	if ( $hi<0 )
-	{
-		$hi = ~$hi;
-		$lo = ~$lo;
-		$c = 1;
-		$neg = "-";
-	}
-
-	$hi = sprintf ( "%u", $hi );
-	$lo = sprintf ( "%u", $lo );
-
-	// x32, bcmath
-	if ( function_exists("bcmul") )
-		return $neg . bcadd ( bcadd ( $lo, bcmul ( $hi, "4294967296" ) ), $c );
-
-	// x32, no-bcmath
-	$hi = (float)$hi;
-	$lo = (float)$lo;
-
-	$q = floor($hi/10000000.0);
-	$r = $hi - $q*10000000.0;
-	$m = $lo + $r*4967296.0;
-	$mq = floor($m/10000000.0);
-	$l = $m - $mq*10000000.0 + $c;
-	$h = $q*4294967296.0 + $r*429.0 + $mq;
-	if ( $l==10000000 )
-	{
-		$l = 0;
-		$h += 1;
-	}
-
-	$h = sprintf ( "%.0f", $h );
-	$l = sprintf ( "%07.0f", $l );
-	if ( $h=="0" )
-		return $neg . sprintf( "%.0f", (float)$l );
-	return $neg . $h . $l;
-}
-
-
-function sphFixUint ( $value )
-{
-	if ( PHP_INT_SIZE>=8 )
-	{
-		// x64 route, workaround broken unpack() in 5.2.2+
-		if ( $value<0 ) $value += (1<<32);
-		return $value;
-	}
-	else
-	{
-		// x32 route, workaround php signed/unsigned braindamage
-		return sprintf ( "%u", $value );
-	}
-}
-
-
 /// sphinx searchd client class
-class SphinxClient
+class SphinxSearchApi_Client
 {
 /// known searchd commands
     const SEARCHD_COMMAND_SEARCH = 0;
@@ -470,6 +209,8 @@ class SphinxClient
 		$this->_mbenc		= "";
 		$this->_arrayresult	= false;
 		$this->_timeout		= 0;
+        
+        $this->Utils = new SphinxSearchApi_Utils;
 	}
 
 	function __destruct()
@@ -999,7 +740,7 @@ class SphinxClient
 			$req .= pack ( "N", (int)$weight );
 		$req .= pack ( "N", strlen($index) ) . $index; // indexes
 		$req .= pack ( "N", 1 ); // id64 range marker
-		$req .= sphPackU64 ( $this->_min_id ) . sphPackU64 ( $this->_max_id ); // id64 range
+		$req .= $this->Utils->sphPackU64 ( $this->_min_id ) . $this->Utils->sphPackU64 ( $this->_max_id ); // id64 range
 
 		// filters
 		$req .= pack ( "N", count($this->_filters) );
@@ -1012,11 +753,11 @@ class SphinxClient
 				case self::SPH_FILTER_VALUES:
 					$req .= pack ( "N", count($filter["values"]) );
 					foreach ( $filter["values"] as $value )
-						$req .= sphPackI64 ( $value );
+						$req .= $this->Utils->sphPackI64 ( $value );
 					break;
 
 				case self::SPH_FILTER_RANGE:
-					$req .= sphPackI64 ( $filter["min"] ) . sphPackI64 ( $filter["max"] );
+					$req .= $this->Utils->sphPackI64 ( $filter["min"] ) . $this->Utils->sphPackI64 ( $filter["max"] );
 					break;
 
 				case self::SPH_FILTER_FLOATRANGE:
@@ -1076,11 +817,11 @@ class SphinxClient
 				assert ( is_numeric($id) );
 				assert ( is_numeric($val) );
 
-				$req .= sphPackU64 ( $id );
+				$req .= $this->Utils->sphPackU64 ( $id );
 				switch ( $entry["type"] )
 				{
 					case self::SPH_ATTR_FLOAT:	$req .= $this->_PackFloat ( $val ); break;
-					case self::SPH_ATTR_BIGINT:	$req .= sphPackI64 ( $val ); break;
+					case self::SPH_ATTR_BIGINT:	$req .= $this->Utils->sphPackI64 ( $val ); break;
 					default:				$req .= pack ( "N", $val ); break;
 				}
 			}
@@ -1204,7 +945,7 @@ class SphinxClient
 				// parse document id and weight
 				if ( $id64 )
 				{
-					$doc = sphUnpackU64 ( substr ( $response, $p, 8 ) ); $p += 8;
+					$doc = $this->Utils->sphUnpackU64 ( substr ( $response, $p, 8 ) ); $p += 8;
 					list(,$weight) = unpack ( "N*", substr ( $response, $p, 4 ) ); $p += 4;
 				}
 				else
@@ -1212,7 +953,7 @@ class SphinxClient
 					list ( $doc, $weight ) = array_values ( unpack ( "N*N*",
 						substr ( $response, $p, 8 ) ) );
 					$p += 8;
-					$doc = sphFixUint($doc);
+					$doc = $this->Utils->sphFixUint($doc);
 				}
 				$weight = sprintf ( "%u", $weight );
 
@@ -1229,7 +970,7 @@ class SphinxClient
 					// handle 64bit ints
 					if ( $type==self::SPH_ATTR_BIGINT )
 					{
-						$attrvals[$attr] = sphUnpackI64 ( substr ( $response, $p, 8 ) ); $p += 8;
+						$attrvals[$attr] = $this->Utils->sphUnpackI64 ( substr ( $response, $p, 8 ) ); $p += 8;
 						continue;
 					}
 
@@ -1251,7 +992,7 @@ class SphinxClient
 						while ( $nvalues-->0 && $p<$max )
 						{
 							list(,$val) = unpack ( "N*", substr ( $response, $p, 4 ) ); $p += 4;
-							$attrvals[$attr][] = sphFixUint($val);
+							$attrvals[$attr][] = $this->Utils->sphFixUint($val);
 						}
 					} else if ( $type==self::SPH_ATTR_MULTI64 )
 					{
@@ -1259,7 +1000,7 @@ class SphinxClient
 						$nvalues = $val;
 						while ( $nvalues>0 && $p<$max )
 						{
-							$attrvals[$attr][] = sphUnpackI64 ( substr ( $response, $p, 8 ) ); $p += 8;
+							$attrvals[$attr][] = $this->Utils->sphUnpackI64 ( substr ( $response, $p, 8 ) ); $p += 8;
 							$nvalues -= 2;
 						}
 					} else if ( $type==self::SPH_ATTR_STRING )
@@ -1268,7 +1009,7 @@ class SphinxClient
 						$p += $val;
 					} else
 					{
-						$attrvals[$attr] = sphFixUint($val);
+						$attrvals[$attr] = $this->Utils->sphFixUint($val);
 					}
 				}
 
@@ -1565,7 +1306,7 @@ class SphinxClient
 		$req .= pack ( "N", count($values) );
 		foreach ( $values as $id=>$entry )
 		{
-			$req .= sphPackU64 ( $id );
+			$req .= $this->Utils->sphPackU64 ( $id );
 			foreach ( $entry as $v )
 			{
 				$req .= pack ( "N", $mva ? count($v) : $v );
